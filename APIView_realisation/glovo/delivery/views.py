0e0a9b180b338911d81a4e50b15011120ddddb52
beketx <beketsk@gmail.com>
2020-04-18 00:02:52 +0600

    DRF

--- APIView_realisation/glovo/delivery/views.py
+++ APIView_realisation/glovo/delivery/views.py
@@ -58,26 +58,26 @@ class ConusmerView(ListModelMixin,GenericAPIView):
         return self.create(request,*args,**kwargs)
 '''
 
-                                                                                                    '''
-                                                                                                    from rest_framework.generics import get_object_or_404
-                                                                                                    from rest_framework.generics import GenericAPIView
-                                                                                                    from rest_framework.mixin import ListModelMixin, CreateModelMixin
-                                                                                                    from .models import *
-                                                                                                    from .serializers import *
+                                                                                                '''
+                                                                                                from rest_framework.generics import get_object_or_404
+                                                                                                from rest_framework.generics import GenericAPIView
+                                                                                                from rest_framework.mixin import ListModelMixin, CreateModelMixin
+                                                                                                from .models import *
+                                                                                                from .serializers import *
 
-                                                                                                    class ConsumerView(ListModelMixin,CreateModelMixin, GenericAPIView):
-                                                                                                        def get(self,request,*args,**kwargs):
-                                                                                                            return self.list(request,*args, **kwargs)
+                                                                                                class ConsumerView(ListModelMixin,CreateModelMixin, GenericAPIView):
+                                                                                                    def get(self,request,*args,**kwargs):
+                                                                                                        return self.list(request,*args, **kwargs)
 
-                                                                                                        def perform_create(self,serializer):
-                                                                                                            # consumer = Consumer.objects.get(id = request.data.get('id'))
-                                                                                                            # return serializer.save(consumer=consumer)
-                                                                                                            author = get_object_or_404(Author, id = self.request.data.get('author_id'))
-                                                                                                            return serializer.save(author=author)
+                                                                                                    def perform_create(self,serializer):
+                                                                                                        # consumer = Consumer.objects.get(id = request.data.get('id'))
+                                                                                                        # return serializer.save(consumer=consumer)
+                                                                                                        author = get_object_or_404(Author, id = self.request.data.get('author_id'))
+                                                                                                        return serializer.save(author=author)
 
-                                                                                                        def post(self,request,*args,**kwargs):
-                                                                                                            return self.create(requet,*args,**kwargs)
-                                                                                                    '''
+                                                                                                    def post(self,request,*args,**kwargs):
+                                                                                                        return self.create(requet,*args,**kwargs)
+                                                                                                '''
 
 #ListApiView CreateApiView
 '''
